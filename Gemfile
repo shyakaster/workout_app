6afdb272f534008f14ebfe8e222860eabec02dff
shyakaster <shyakaster@gmail.com>
2015-09-30 18:21:04 +0300

    add bootstrap and style home page

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '4.2.0'
 gem 'mysql2', '~> 0.3.18'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+#gems for installing bootstrap
+gem 'bootstrap-sass', '~> 3.3.5.1'
+gem 'autoprefixer-rails', '~> 6.0.2'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- spec/features/creating_home_page_spec.rb
+++ spec/features/creating_home_page_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature "Creating home page" do
   scenario "create a home page" do
     visit '/'
-    expect(page).to have_content("Athletes Den")
-    expect(page).to have_content "Home"
+    expect(page).to have_link("Athletes Den")
+    expect(page).to have_link "Home"
     expect(page).to have_content("Workout Lounge")
   end
 end
