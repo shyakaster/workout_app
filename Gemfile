3162639ee8579da1249cc1c0f131b72fac0a39ea
shyakaster <shyakaster@gmail.com>
2015-10-03 12:52:15 +0300

    add date-picker and list workouts

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.2.0'
 gem 'mysql2', '~> 0.3.18'
 #Use devise for user authentication
 gem 'devise', '~> 3.5.2'
+gem 'jquery-ui-rails', '~> 5.0.5'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 #gems for installing bootstrap
---
+++ spec/features/listing_exercises_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Listing exercises" do
+  before do
+    @john=User.create(email:"shyakaster@gmail.com",password:"password")
+    login_as @john
+    @e1=@john.exercises.create(duration_in_min:20, workout:"Body building",workout_date:"2015-2-4")
+    @e2=@john.exercises.create(duration_in_min:14, workout:"Body building and boxing",workout_date:"2015-12-14")
+  end
+  scenario "shows users workout for the last seven days" do
+    visit '/'
+
+    click_link "My Lounge"
+
+    expect(page).to have_content(@e1.duration_in_min)
+    expect(page).to have_content(@e1.workout)
+    expect(page).to have_content(@e1.workout_date)
+
+    expect(page).to have_content(@e2.duration_in_min)
+    expect(page).to have_content(@e2.workout)
+    expect(page).to have_content(@e2.workout_date)
+
+  end
+end
