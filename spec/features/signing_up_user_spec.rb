ed2440ce2bbdc88427ceb9e4f626679d5e48c2fa
shyakaster <shyakaster@gmail.com>
2015-10-05 14:19:10 +0300

    add usernames to users

--- spec/features/signing_up_user_spec.rb
+++ spec/features/signing_up_user_spec.rb
@@ -4,10 +4,14 @@ RSpec.feature "Users sign up" do
   scenario "with valid credentials"do
   visit "/"
   click_link "Sign Up"
+  fill_in "First name", with:"John"
+  fill_in "Last name", with:"Doe"
   fill_in "Email",with: "shyakaster@gmail.com"
   fill_in "Password",with: "password"
   fill_in "Password confirmation",with: "password"
   click_button "Sign up"
   expect(page).to have_content("You have signed up successfully")
+    visit '/'
+    expect(page).to have_content("John Doe")
  end
 end
