0bdacbea53096f50aedd40d6012e3aca0373af71
shyakaster <shyakaster@gmail.com>
2015-10-05 13:45:51 +0300

    list members

---
+++ spec/features/listing_member_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Listing members" do
+  before do
+    @john=User.create(first_name:"John",last_name:"Doe",email:"shyakaster@gmail.com",password:"password")
+    @sarah=User.create(first_name:"Sarah",last_name:"Joseph",email:"john@gmail.com",password:"password")
+  end
+  scenario "only registered users" do
+    visit '/'
+    expect(page).to have_content("List of Members")
+    expect(page).to have_content(@john.full_name)
+    expect(page).to have_content(@sarah.full_name)
+  end
+end
